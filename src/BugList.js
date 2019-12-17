ef444d84f4ca3c988d103359b627116153642c24
hadz333 <dhadzic@uw.edu>
2019-12-17 11:15:10 -0800

    remove button working

--- src/BugList.js
+++ src/BugList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import ListGenerator from './ListGenerator';
+import Table from 'react-bootstrap/Table';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 class BugList extends React.Component {
   constructor(props) {
@@ -14,7 +16,42 @@ class BugList extends React.Component {
     return (
       <div>
         <h3>Bug Tracker</h3>
-        <ListGenerator items={this.state.items} />
+        <Table striped bordered hover>
+	      <thead>
+		    <tr>
+		      <th>#</th>
+		      <th>Bug details</th>
+		      <th>Reported by</th>
+		      <th>Date</th>
+		      <th>Status</th>
+		   
+		    </tr>
+		  </thead>
+		  <tbody>
+		  	
+	        
+		  
+	    
+	       {this.state.items.map((item,index) => 
+	     	<tr key={index}>
+	     	<td>{index+1}</td>
+	     	<td>{item.text}</td>
+	     	<td>{item.name}</td>
+	     	<td>{Date(item.id).toString().slice(0, 25)}</td>
+	     	<td><DropdownButton id="status-dropdown" title="Unresolved" variant="warning">
+	  			<Dropdown.Item href="#/action-1">Unresolved</Dropdown.Item>
+	  			<Dropdown.Item href="#/action-2">In progress</Dropdown.Item>
+	  			<Dropdown.Item href="#/action-3">Resolved</Dropdown.Item>
+				</DropdownButton></td>
+			<td><Button id={index} variant="danger" type="button" onClick={this.handleChange}>
+	            Remove
+	          </Button></td>
+	     	</tr> 
+	       )}
+
+	        
+	      </tbody>
+	      </Table>
         <br />
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="new-todo">
@@ -41,7 +78,8 @@ class BugList extends React.Component {
             Report Bug
           </Button>
         </form>
-      </div>
+	      
+	    </div>
     );
   }
   handleChange(e) {
@@ -49,8 +87,24 @@ class BugList extends React.Component {
     	this.setState({ text: e.target.value });
 	} else if (e.target.id === "name") {
 		this.setState({ name: e.target.value });
-	} else {
-		console.log('hey');
+	} else if (e.target.type === "button") {
+		console.log(e.target.id);
+		const index = e.target.id;
+		if (this.state.items.length <= 1) {
+			this.setState(state => ({
+		      items: [],
+		      text: '',
+		      name: '',
+		    }));
+		} else {
+			this.state.items.splice(index, 1);
+			this.setState(state => ({
+		      items: this.state.items
+		    }));
+		}
+		
+	    console.log(this.state.items);
+
 	}
   }
 
