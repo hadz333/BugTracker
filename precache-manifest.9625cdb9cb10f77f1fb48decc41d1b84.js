151f435b306af2e801172d4576a63eb589a03697
hadz333 <dhadzic@uw.edu>
2019-12-17 22:30:39 -0800

    Updates

---
+++ precache-manifest.9625cdb9cb10f77f1fb48decc41d1b84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22afd0d72c17fd1965e7922f56716bf",
+    "url": "/BugTracker/index.html"
+  },
+  {
+    "revision": "ba0f7c48b7a045340eb6",
+    "url": "/BugTracker/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "174fa42d3cbc84b4ddcd",
+    "url": "/BugTracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ba0f7c48b7a045340eb6",
+    "url": "/BugTracker/static/js/2.67fa2c01.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/BugTracker/static/js/2.67fa2c01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "174fa42d3cbc84b4ddcd",
+    "url": "/BugTracker/static/js/main.edbf104d.chunk.js"
+  },
+  {
+    "revision": "9dae2be65e0d4fb4fc58",
+    "url": "/BugTracker/static/js/runtime-main.c4bbfbf5.js"
+  }
+]);
